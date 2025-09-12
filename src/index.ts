5a6f40bf242109f7aab1ba29837743ce3a5fc642
Ben Brady <benbrady04.bb@gmail.com>
2025-09-12 21:35:10 +0100

    0.5.0: removed createContextHook due to jsx errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { useAbortSignal } from "./useAbortSignal.js";
 export { useIsClient } from "./useIsClient.js";
-export { createContextHook } from "./context.jsx";
 export { createStoredSignal, createCustomSignalStore } from "./createStoreSignal.js";
 export {
     useDeltaFrameInterval,
