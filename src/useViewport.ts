92a064fb439288e8b5e37a4e0a2c425c48209df5
Ben Brady <benbrady04.bb@gmail.com>
2025-10-01 12:01:08 +0100

    fix(useVIewport): updated to work

--- src/useViewport.ts
+++ src/useViewport.ts
@@ -1,50 +1,34 @@
-import { batch, createSignal, type Accessor } from "solid-js";
-import { useAbortSignal } from "./useAbortSignal.js";
+import { batch, createSignal, onCleanup, type Accessor } from "solid-js";
 import { onClient } from "./onClient.js";
 
 export const useViewportSize = (): [Accessor<number | undefined>, Accessor<number | undefined>] => {
     let [width, setWidth] = createSignal<number | undefined>();
     let [height, setHeight] = createSignal<number | undefined>();
-    let signal = useAbortSignal();
 
-    let update = () =>
-        batch(() => {
-            setHeight(document.body.clientHeight);
-            setWidth(document.body.clientWidth);
-        });
-
-    onClient(() => {
-        update();
-        document.body.addEventListener("resize", update, { signal });
-    });
+    useSizeChange(() =>
+        batch(() => (setHeight(document.body.clientHeight), setWidth(document.body.clientWidth))),
+    );
 
     return [width, height];
 };
 
 export const useViewportWidth = (): Accessor<number | undefined> => {
     let [width, setWidth] = createSignal<number | undefined>();
-    let signal = useAbortSignal();
-
-    let update = () => setWidth(document.body.clientWidth);
-
-    onClient(() => {
-        update();
-        document.body.addEventListener("resize", update, { signal });
-    });
-
+    useSizeChange(() => setWidth(document.body.clientWidth));
     return width;
 };
 
 export const useViewportHeight = (): Accessor<number | undefined> => {
     let [height, setHeight] = createSignal<number | undefined>();
-    let signal = useAbortSignal();
-
-    let update = () => setHeight(document.body.clientHeight);
-
-    onClient(() => {
-        update();
-        document.body.addEventListener("resize", update, { signal });
-    });
-
+    useSizeChange(() => setHeight(document.body.clientHeight));
     return height;
+};
+
+const useSizeChange = (callback: () => void) => {
+    let observer = new ResizeObserver(callback);
+    onClient(() => {
+        callback();
+        observer.observe(document.body);
+    });
+    onCleanup(() => observer.disconnect());
 };
